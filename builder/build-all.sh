819f3be44e059bead70b7efe94b4da39e6beecdf
Frank Hunleth <fhunleth@troodon-software.com>
2019-11-25 08:46:08 -0500

    Make build-all.sh script friendlier to rebuilds

--- builder/build-all.sh
+++ builder/build-all.sh
@@ -2,6 +2,9 @@
 
 configs=$(find ./configs -name "*_defconfig")
 
+# Make sure that local build products don't affect these builds.
+rm -f ../src/*.o
+
 # Configure everything
 for config in $configs; do
     echo "Creating $config..."
@@ -17,6 +20,11 @@ for config in $configs; do
     base=$(basename -s _defconfig $config)
 
     echo "Building $base..."
+
+    # If rebuilding, force a rebuild of rootfs_mounter and undo the prune of /usr/lib
+    # that breaks the os-release copy script.
+    mkdir -p o/$base/target/usr/lib
+    rm -fr o/$base/build/rootfs_mounter
     make -C o/$base
     if [[ $? != 0 ]]; then
         echo "--> 'Building $base' failed!"
