bd41d4f62bf291dc4804d00058376783767b7a01
Frank Hunleth <fhunleth@troodon-software.com>
2019-12-14 16:47:40 -0500

    Fix cpio and compression options
    The most important fix is to the xz options since using crc64 (the
    default) appears to be unsupported by the Linux kernel's xz
    decompressor.
    
    The other options are for reproducibility and max compression.

--- builder/board/nerves_initramfs/post-build.sh
+++ builder/board/nerves_initramfs/post-build.sh
@@ -5,11 +5,18 @@ set -e
 # The default configurations only need init to be in the archive so
 # create the rootfs.cpio manually to only include it.
 mkdir -p "$BINARIES_DIR"
-cd "$TARGET_DIR" && echo init | cpio -o -H newC --owner=root:root > "$BINARIES_DIR/rootfs.cpio"
+cd "$TARGET_DIR" && echo init | cpio -o -H newC --owner=root:root --reproducible --quiet > "$BINARIES_DIR/rootfs.cpio"
 
+# Notes on compressor options:
+#
+# 1. -9 -> max compression
+# 2. -f -> force
+# 3. -k -> keep rootfs.cpio
+# 4. xz -C crc32 -> Linux kernel doesn't seem to support the default check (crc64)
+# 5. gzip -n -> Don't store original filename
 cd "$BINARIES_DIR" &&
     (
-        gzip -k rootfs.cpio
-        xz -k rootfs.cpio
+        gzip -9 -n -f -k rootfs.cpio
+        xz -9 -C crc32 -f -k rootfs.cpio
     )
 
