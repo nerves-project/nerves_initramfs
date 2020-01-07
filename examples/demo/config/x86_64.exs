b22890a0e2723f1d8707cf81a8a91094187ea786
Frank Hunleth <fhunleth@troodon-software.com>
2020-01-07 17:01:29 -0500

    Fix the ctty used for the demo

--- examples/demo/config/x86_64.exs
+++ examples/demo/config/x86_64.exs
@@ -1,4 +1,5 @@
 use Mix.Config
 
-config :nerves, :firmware,
-  fwup_conf: "config/fwup-x86_64.conf"
+config :nerves, :firmware, fwup_conf: "config/fwup-x86_64.conf"
+
+config :nerves, :erlinit, ctty: "ttyS0"
