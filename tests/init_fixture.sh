a73d94188d975fd036bf7800f174f58074bf3ed8
Frank Hunleth <fhunleth@troodon-software.com>
2020-01-08 11:02:52 -0500

    Fix most shellcheck warnings

--- tests/init_fixture.sh
+++ tests/init_fixture.sh
@@ -6,37 +6,38 @@
 # $INIT
 # $TESTS_DIR
 
-mkdir -p $WORK/dev
-ln -s $INIT $WORK/init
-mkdir -p $WORK/sbin
-mkdir -p $WORK/usr/bin
-ln -s $TESTS_DIR/fake_init $WORK/sbin/init
-ln -s $TESTS_DIR/fake_fwup $WORK/usr/bin/fwup
-ln -s $TESTS_DIR/fake_boardid $WORK/usr/bin/boardid
-ln -s $TESTS_DIR/fake_faulty_program $WORK/usr/bin/faulty_program
+mkdir -p "$WORK/dev"
+ln -s "$INIT" "$WORK/init"
+mkdir -p "$WORK/sbin"
+mkdir -p "$WORK/usr/bin"
+ln -s "$TESTS_DIR/fake_init" "$WORK/sbin/init"
+ln -s "$TESTS_DIR/fake_fwup" "$WORK/usr/bin/fwup"
+ln -s "$TESTS_DIR/fake_boardid" "$WORK/usr/bin/boardid"
+ln -s "$TESTS_DIR/fake_faulty_program" "$WORK/usr/bin/faulty_program"
 
 # Create the device containing a root filesystem
-dd if=/dev/zero of=$WORK/dev/mmcblk0p2 bs=512 count=0 seek=1024 2>/dev/null
-ln -s /dev/null $WORK/dev/null
+dd if=/dev/zero of="$WORK/dev/mmcblk0p2" bs=512 count=0 seek=1024 2>/dev/null
+ln -s /dev/null "$WORK/dev/null"
 
 # Fake active console
-ln -s $(tty) $WORK/dev/ttyF1
-ln -s $(tty) $WORK/dev/ttyAMA0
-ln -s $(tty) $WORK/dev/tty1
+real_tty="$(tty)"
+ln -s "$real_tty" "$WORK/dev/ttyF1"
+ln -s "$real_tty" "$WORK/dev/ttyAMA0"
+ln -s "$real_tty" "$WORK/dev/tty1"
 
 # Loop and dm-crypt
-mkdir -p $WORK/dev/mapper
-touch $WORK/dev/mapper/control
-touch $WORK/dev/loop0
+mkdir -p "$WORK/dev/mapper"
+touch "$WORK/dev/mapper/control"
+touch "$WORK/dev/loop0"
 
 # "Block" devices
-mkdir -p $WORK/sys/block
-touch $WORK/sys/block/mmcblk0
-touch $WORK/sys/block/sda
-ln -s $TESTS_DIR/gpt-disk.img $WORK/dev/mmcblk0
-touch $WORK/dev/mmcblk0p1
-touch $WORK/dev/mmcblk0p2
-touch $WORK/dev/mmcblk0p5
-ln -s $TESTS_DIR/mbr-disk.img $WORK/dev/sda
-touch $WORK/dev/sda1
-touch $WORK/dev/sda2
+mkdir -p "$WORK/sys/block"
+touch "$WORK/sys/block/mmcblk0"
+touch "$WORK/sys/block/sda"
+ln -s "$TESTS_DIR/gpt-disk.img" "$WORK/dev/mmcblk0"
+touch "$WORK/dev/mmcblk0p1"
+touch "$WORK/dev/mmcblk0p2"
+touch "$WORK/dev/mmcblk0p5"
+ln -s "$TESTS_DIR/mbr-disk.img" "$WORK/dev/sda"
+touch "$WORK/dev/sda1"
+touch "$WORK/dev/sda2"
--- tests/repl.sh
+++ tests/repl.sh
@@ -8,45 +8,39 @@ readlink_f () {
     if [ -h "$filename" ]; then
         readlink_f "$(readlink "$filename")"
     else
-        echo "`pwd -P`/$filename"
+        echo "$(pwd -P)/$filename"
     fi
 }
 
-TESTS_DIR=$(dirname $(readlink_f $0))
+TESTS_DIR=$(dirname $(readlink_f "$0"))
 
 WORK=$TESTS_DIR/work
 
 INIT=$TESTS_DIR/../src/init
 FIXTURE=$TESTS_DIR/fixture/init_fixture.so
 
-CHAINED_INIT=$TESTS_DIR/fake_init
-
-# Just in case there are some leftover from a previous test, clear it out
-rm -fr $WORK
-
-if [ ! -f $INIT ]; then echo "Build $INIT first"; exit 1; fi
-if [ ! -f $FIXTURE ]; then echo "Build $FIXTURE first"; exit 1; fi
+if [ ! -f "$INIT" ]; then echo "Build $INIT first"; exit 1; fi
+if [ ! -f "$FIXTURE" ]; then echo "Build $FIXTURE first"; exit 1; fi
 
 run() {
-    CMDLINE=$*
     CONFIG=$WORK/nerves_initramfs.conf
 
     # Setup a fake root directory to simulate init boot
-    rm -fr $WORK
+    rm -fr "$WORK"
     source "$TESTS_DIR/init_fixture.sh"
 
     # Run the test script to setup files for the test
-    cat >$CONFIG <<EOF
+    cat >"$CONFIG" <<EOF
         run_repl = true
 EOF
 
     # Run init
     # NOTE: Call 'exec' so that it's possible to set argv0, but that means we
     #       need a subshell - hence the parentheses.
-    (LD_PRELOAD=$FIXTURE DYLD_INSERT_LIBRARIES=$FIXTURE WORK=$WORK exec -a /init $INIT $CMDLINE)
+    (LD_PRELOAD=$FIXTURE DYLD_INSERT_LIBRARIES=$FIXTURE WORK=$WORK exec -a /init "$INIT")
 }
 
 run
 
-rm -fr $WORK
+rm -fr "$WORK"
 exit 0
